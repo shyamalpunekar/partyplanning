ab23794487a66817b697d52f4de426f189205f1e
shyamalpunekar <shyamal.punekar@gmail.com>
2017-08-04 11:49:22 -0700

    Add branching for food check with user input 'NO'

--- Desktop/Epicodus-Java/partyplanning/src/main/java/App.java
+++ Desktop/Epicodus-Java/partyplanning/src/main/java/App.java
@@ -20,11 +20,28 @@ public class App {
                 "your choice. Answer these Questions. Would you like us to have party for you? YES or NO");
 
         try {
-            while (mainAppRunning){
+            while (mainAppRunning) {
                 String userAnswer = bufferedReader.readLine().toUpperCase();
-                
-            }
+                if (userAnswer.equals("NO")) {
+                    System.out.println("How many guests are you expecting at your party? Enter number");
+                    guests = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("Please choose one of the following for your food service: full course, light meal or snacks");
+                    boolean checkFood = true;
+                    while (checkFood) {
+                        food = bufferedReader.readLine().toLowerCase();
+                        if (food.equals("full course") || food.equals("light meal") || food.equals("snacks")) {
+                            checkFood = false;
+                        } else {
+                            System.out.println("Sorry, we don't recognize your input. Please enter one of the following: " +
+                                    "full course, light meal, snacks.");
+                        }
+                    }
+                    System.out.println("Please choose one of the options for beverages: ull bar, light refreshments, water");
 
+
+                }
+
+            }
         }
         catch (IOException e){
             e.printStackTrace();
